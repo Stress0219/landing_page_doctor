e81a162e19f11fe2ff5b34aff524f487a42fddf3
Stress0219 <joan.comas12@gmail.com>
2023-06-13 07:55:40 -0400

    hover_effect_added

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
 
 <div className="flex flex-wrap justify-evenly">    
 <div
-  className="flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  className="transition duration-300 ease-in hover:scale-105 flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
   <img
     className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
     src="/tratos.jpg"
@@ -41,7 +41,7 @@ function App() {
   </div>  
 </div>
 <div
-  className="flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  className="transition duration-300 ease-in hover:scale-105 flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
   <img
     className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
     src="/investigacion.jpg"
@@ -54,7 +54,7 @@ function App() {
   </div>
 </div>
 <div
-  className="flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  className="transition duration-300 ease-in hover:scale-105 flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
   <img
     className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
     src="/consulta.jpg"
