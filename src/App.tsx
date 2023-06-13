d1f1cb227c8852fbea6a3ef87d0232fab588180a
Stress0219 <joan.comas12@gmail.com>
2023-06-13 06:31:35 -0400

    cards_added

--- src/App.tsx
+++ src/App.tsx
@@ -25,46 +25,54 @@ function App() {
   <div className="absolute top-0 left-0 w-full h-full bg-blue-500 opacity-30"></div>
 </div>
 
+<div className="flex">    
+<div
+  className="flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  <img
+    className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+    src=""
+    alt="otorrinolaringologo en monterrey" />
+  <div className="flex flex-col justify-start p-6">
+    <h2 className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+    Te mereces un alto nivel de atención por parte de un Otorrinolaringólogo de vanguardia,
+     por lo que el Dr. Jorge Treviño Garza especialista en padecimiento de los oídos.
+    </h2>
+  </div>  
+</div>
+<div
+  className="flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  <img
+    className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+    src=""
+    alt="otorrinolaringologo en monterrey" />
+  <div className="flex flex-col justify-start p-6">
+    <h2 className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+    El otorrinolaringólogo en Monterrey Jorge Treviño es ahora uno de los médicos más importantes en su campo
+     gracias a su contribución a la investigación y la prevención.
+    </h2>
+  </div>
+</div>
+<div
+  className="flex flex-col mx-4 my-2 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row">
+  <img
+    className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+    src=""
+    alt="otorrinolaringologo en monterrey" />
+  <div className="flex flex-col justify-start p-6">
+    <h2 className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+      <ul>
+   <li>⦁	Consulta</li>
+   <li>⦁	Radiofrecuencia de Cornetes</li>
+   <li>⦁	Cirugia endoscópica para sinusitis</li>
+   <li>⦁	Cirugia de Tabique nasal</li>
+   <li>⦁	Endoscopia Nasal</li>
+   <li>⦁	Otorrinolaringología Pediátrica</li>
+</ul>
+    </h2>
+  </div> 
+</div>
 
-
-      <div className="bg-slate-100">
-        <div>
-          <h1 className="ml-2 mb-2 text-lg">
-              El <strong>Dr. Jorge Treviño Garza</strong> es un <strong>Otorrinolaringólogo en Monterrey</strong> que cuenta con especialidad en Cirugía Plástica Facial, es un médico que constantemente se actualiza con el  fin de mejorar sus servicios a los pacientes. 
-              Servicios del <strong>Otorrinolaringólogo en Monterrey</strong>. 
-         </h1>
-         <ul className="font-semibold ml-2">
-          <li>- Consulta</li>
-          <li>- Radiofrecuencia de Cornetes</li>
-          <li>- Cirugia endoscópica para sinusitis</li>
-          <li>- Cirugia de Tabique nasal</li>
-          <li>- Endoscopia Nasal</li>
-          <li>- Otorrinolaringología Pediátrica</li>
-         </ul>
-         <h2 className="ml-2 mt-2 mb-4">
-            Te mereces un alto nivel de atención por parte de un <strong>Otorrinolaringólogo</strong> de vanguardia, por lo que el Dr. Jorge Treviño Garza especialista en padecimiento de los oídos.
-         </h2>
-         <p>
-         Agenda una consulta por whatsapp al número  8115448285
-          o mediante una llamada al 8183475220 <br />
-          nuestro correo: <a href="mailto:jorgetrega@gmail.com">jorgetrega@gmail.com</a> <br />
-          Lunes a Sábado <br />
-          9:00 a 19:00 hr
-         </p>
-        </div>
-
-        <div className="footer" id="contact">
-      Visitanos para que seas atendido con la atención que te mereces en  
-       Centro de Especialidades Médicas
-      1er piso consultorio 126 Jose Benitez No 2704 Colonia obispado Monterrey NL CP 64060
-   
-      El otorrinolaringólogo en Monterrey Jorge Treviño es ahora uno de los médicos más importantes en su campo gracias a su contribución a la investigación y la prevención.
-
-
-      Da clic en el botón de WhatsApp para contactarnos para saber más sobre el otorrinolaringólogo en Monterrey.
-
-        </div>
-      </div>
+</div>
     </div>
   )
 }
